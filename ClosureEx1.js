53aba8600102ea3b45e62f08a506f7b6a8aed992
ayashadevikar <ayashadevikar8@gmail.com>
2022-12-06 16:15:37 +0530

    Add some changes for 6-12-2022 file

---
+++ ClosureEx1.js
@@ -0,0 +1,10 @@
+function printFullName(firstName, lastName){
+    function printName(){
+        console.log(firstName, lastName);
+    }
+    return printName;
+}
+
+const ans = printFullName("alex", "cart");
+// console.log(ans);
+ans();
