e99f2aaa34cc656e8234eb0a90b67f363ba0dd75
ayashadevikar <ayashadevikar8@gmail.com>
2022-12-09 14:32:21 +0530

    Add some changes for 9-12-2022 file

---
+++ LetConstHositing.js
@@ -0,0 +1,11 @@
+// let and const hoisted
+
+console.log(firstName);
+
+let firstName = "john";
+console.log(firstName);
+
+console.log(firstName);
+
+const firstName2 = "john";
+console.log(firstName);
