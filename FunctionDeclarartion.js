6164cbbd855c220db1e5e3873343ecefe1428f8d
ayashadevikar <ayashadevikar8@gmail.com>
2022-12-12 13:51:44 +0530

    Add some changes for 12-12-2022 file

---
+++ FunctionDeclarartion.js
@@ -0,0 +1,15 @@
+// function declaration
+
+console.log(this);
+console.log(window);
+console.log(myFunction);
+console.log(fullName);
+
+function myFunction(){
+    console.log("this is my function");
+}
+
+var firstName = "john";
+var lastName = "cart";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
