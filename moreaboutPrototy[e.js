ae2f4c789872d9120e09bac3a00481708e1be167
ayashadevikar <ayashadevikar8@gmail.com>
2022-12-19 13:58:42 +0530

    Add some changes for 19-12-2022 file

---
+++ moreaboutPrototy[e.js
@@ -0,0 +1,13 @@
+// more about prototype
+
+// let numbers = new Array(1,2,3);
+// console.log(Array.prototype);
+
+let numbers = [1,2,3];
+console.log(Object.getPrototypeOf(numbers));
+console.log(numbers);
+
+// function hello(){
+//     console.log("hello");
+// }
+// hello();
