8bbe08ed2347150f9a473257bf208cc55b15ffff
ayashadevikar <ayashadevikar8@gmail.com>
2022-12-21 13:13:39 +0530

    Add some changes for 21-12-2022 file

---
+++ NewKeyword.js
@@ -0,0 +1,30 @@
+// new keyword
+
+
+function CreateUser(firstName, lastName, email, age, address){
+    
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+  
+}
+
+
+CreateUser.prototype.about =  function(){
+            return `${this.firstName} is ${this.age} years `
+        };
+ CreateUser.prototype.is18 =   function(){
+            return this.age >= 18;
+        }; 
+ CreateUser.prototype.sing = function(){
+            return 'toon na na na la la';
+        }       
+
+
+
+
+const user1 = new CreateUser('alex', 'scott', 'alex@gmail.com', 'spanish');
+console.log(user1);
+console.log(user1.is18());
