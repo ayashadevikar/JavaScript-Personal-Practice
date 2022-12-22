0eb9a644d9dfb6c5536942cda2814d78f649e2a7
ayashadevikar <ayashadevikar8@gmail.com>
2022-12-22 10:43:58 +0530

    Add some changes for 22-12-2022 file

--- UsePrototype.js
+++ UsePrototype.js
@@ -1,19 +1,19 @@
 // Use Prototype
 
-const userMethods = {
-    about : function(){
-        return `${this.firstName} is ${this.age} years `
-    },
-    is18 : function(){
-        return this.age >= 18;
-    },
-    sing: function(){
-        return 'toon na na na la la';
-    }
-}
+// const userMethods = {
+//     about : function(){
+//         return `${this.firstName} is ${this.age} years `
+//     },
+//     is18 : function(){
+//         return this.age >= 18;
+//     },
+//     sing: function(){
+//         return 'toon na na na la la';
+//     }
+// }
 
 function createUser(firstName, lastName, email, age, address){
-    const user = Object.create(userMethods); // {}
+    const user = Object.create(createUser.prototype); // {}
     user.firstName = firstName;
     user.lastName = lastName;
     user.email = email;
@@ -22,5 +22,20 @@ function createUser(firstName, lastName, email, age, address){
     return user;
 }
 
+// console.log(createUser.prototype);
+createUser.prototype.about =  function(){
+            return `${this.firstName} is ${this.age} years `
+        };
+ createUser.prototype.is18 =   function(){
+            return this.age >= 18;
+        }; 
+ createUser.prototype.sing = function(){
+            return 'toon na na na la la';
+        }       
+
+
+
+
 const user1 = createUser('alex', 'scott', 'alex@gmail.com', 'spanish');
 console.log(user1);
+console.log(user1.is18());
