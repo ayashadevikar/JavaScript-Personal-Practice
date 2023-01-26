1b8d49b095c82ace1f2356484f3a053085934b35
ayashadevikar <ayashadevikar8@gmail.com>
2023-01-26 17:12:40 +0530

    js practice folder added

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,11 @@
+var fullName = "Ayasha Devikar";
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+console.log(paymentMode); // undefined output
+
+paymentMode = "Credit Card";
