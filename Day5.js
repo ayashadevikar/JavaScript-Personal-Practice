554119db0a0871954fdeafae72c67beea1e74626
ayashadevikar <ayashadevikar8@gmail.com>
2023-03-06 19:23:24 +0530

    js files added

---
+++ Day5.js
@@ -0,0 +1,13 @@
+// for of in loop
+
+let userName = {
+    firstName: 'alex',
+    age: 22,
+    lastName: 'xander',
+    gender: 'male',
+};
+
+
+for (let x in userName){
+    console.log(userName[x]);
+}
